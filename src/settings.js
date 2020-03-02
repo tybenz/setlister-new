8dee7aaa7169626973f908fd5b0527573e334ddd
Tyler Benziger <tabenziger@gmail.com>
2020-03-01 22:01:14 -0800

    cdn settings pt 3

--- src/settings.js
+++ src/settings.js
@@ -14,7 +14,7 @@ var Settings = {
     },
 
     cdn: {
-        host: 'setlister-prod-ue1-assets.s3.amazonaws.com'
+        host: 'setlister-prod-ue1-assets.s3.amazonaws.com',
         protocol: 'https:'
     }
 };
