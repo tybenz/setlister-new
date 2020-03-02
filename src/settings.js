af259038cb96f1ad54be69b085363464362b31d0
Tyler Benziger <tabenziger@gmail.com>
2020-03-01 21:56:50 -0800

    cdn setting

--- src/settings.js
+++ src/settings.js
@@ -11,6 +11,13 @@ var Settings = {
         dev: 'localhost',
         stage: 'localhost',
         production: 'setlister-new.herokuapp.com'
+    },
+
+    cdn: {
+        local: 'localhost',
+        dev: 'localhost',
+        stage: 'localhost',
+        production: 'setlister-prod-ue1-assets.s3.amazonaws.com'
     }
 };
 
