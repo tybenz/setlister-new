43579c50fc708ae8941f7186c78a44218e65b034
Tyler Benziger <tabenziger@gmail.com>
2020-03-09 12:02:32 -0700

    Handle missing linked song

--- src/models/setlist_song.js
+++ src/models/setlist_song.js
@@ -15,17 +15,17 @@ var SetlistSong = BaseModel.extend({
 
     fullSong: function() {
         var songData = this.related('song').toJSON();
-        songData.start_key = songData.data_key;
+        songData.start_key = songData && songData.data_key;
         songData.data_key = this.get( 'data_key' );
         songData.position = this.get( 'position' );
         songData.capo = this.get( 'capo' );
         songData.setlist_song_id = this.id;
-        songData.song_id = songData.id;
+        songData.song_id = songData && songData.id;
         songData.path = songData && songData.id ? router.songPath(songData.id) : router.songPath(1);
         songData.edit_path = songData && songData.id ? router.editSongPath(songData.id) : router[ 'editSongPath(1);' ]
         songData.setlist_song_path = router.setlistSongPath(this.id);
         songData.delete_path = router.deleteSetlistSongPath(this.id);
-        songData.title_dashes = songData.title.replace(/ /g, '-');
+        songData.title_dashes = songData && songData.title ? songData.title.replace(/ /g, '-') : '';
         return songData;
     }
 });
