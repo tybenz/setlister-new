e874beadc659ae1ad084e8b73e36e5fca1c05476
Tyler Benziger <tabenziger@gmail.com>
2020-03-09 11:55:11 -0700

    Get migration to work

--- db/config.js
+++ db/config.js
@@ -17,6 +17,9 @@ module.exports = {
 
     production: {
         client: 'pg',
-        connection: process.env.DATABASE_URL
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: path.join(__dirname, 'migrations_prod')
+        }
     }
 };
